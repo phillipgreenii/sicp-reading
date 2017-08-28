ca4fe7125e35d47c6f47d75c58f26de4339d2d20
Phillip Green II <phillip.green.ii@gmail.com>
2017-08-27 21:16:52 -0400

    added WIP

---
+++ Exercises/phillipgreenii/exercise-1.14.scm
@@ -0,0 +1,54 @@
+;(count-change 11)
+;  (cc 11 5)
+;    (cc 11 4)
+;      (cc 11 3)
+;        (cc 11 2)
+;          (cc 11 1)
+;          (cc 6 2)
+;            (cc 6 1)
+;              (cc 6 0)
+;                0
+;              (cc 5 1)
+;                (cc 5 0)
+;                  0
+;                (cc 4 1)
+;                  (cc 4 0)
+;                    0
+;                  (cc 3 1)
+;                    (cc 3 0)
+;                      0
+;                    (cc 2 1)
+;                      (cc 2 0)
+;                        0
+;                      (cc 1 1)
+;                        (cc 1 0)
+;                          0
+;                        (cc 0 1)
+;                          1
+;            (cc 1 2)
+;              (cc 1 1)
+;                (cc 1 0)
+;                  0
+;                (cc 0 1)
+;                  1
+;              (cc -4 2)
+;                0
+;        (cc 1 3)
+;          (cc 1 2)
+;            (cc 1 1)
+;              (cc 1 0)
+;                0
+;              (cc 0 1)
+;                1
+;            (cc -4 2)
+;              0
+;          (cc -9 3)
+;            0
+;      (cc -14 4)
+;        0
+;    (cc -39 5)
+;      0
+
+;;growth of space: linear because the solution of all pennies is always valid,
+;; everything else is less
+;;number of steps: 
